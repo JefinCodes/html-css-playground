cdede3ad6557981a0377fba406ee298b06049793
jefin <techy.jefinjoji@gmail.com>
2025-06-20 10:15:25 +0530

    carousel/: add scroll snapping

--- carousel/script.js
+++ carousel/script.js
@@ -2,10 +2,12 @@ const scrollableContainer = document.querySelector('.scroll-container');
 const scrollLeftBtn = document.querySelector('.scroll-left');
 const scrollRightBtn = document.querySelector('.scroll-right');
 
+const scrollAmount = 339 + 20; // card width + gap
+
 scrollLeftBtn.addEventListener('click', () => {
-    scrollableContainer.scrollBy({left: -359, behavior: 'smooth'}); // left = width + gap
+    scrollableContainer.scrollBy({left: -scrollAmount, behavior: 'smooth'}); // left = width + gap
 });
 
 scrollRightBtn.addEventListener('click', () => {
-    scrollableContainer.scrollBy({left: 359, behavior: 'smooth'}); // left = width + gap
+    scrollableContainer.scrollBy({left: scrollAmount, behavior: 'smooth'}); // left = width + gap
 });
